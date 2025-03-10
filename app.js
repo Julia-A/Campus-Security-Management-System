146ebf6b61dd184f778e3b12eb1218a410a837cf
Julia <juliaaderemi@gmail.com>
2025-03-10 18:00:20 +0100

    more commits

--- app.js
+++ app.js
@@ -224,14 +224,15 @@ const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: parseInt(process.env.EMAIL_PORT),
   secure: false,
-  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   },
-  tls: {
-    rejectUnauthorized: false // For development environments
-  }
+  // tls: {
+  //   rejectUnauthorized: false // For development environments
+  // },
+  // debug: true, // Enable debug
+  // logger: true  // Log email sending attempts
 });
 
 // Function to send notification emails
